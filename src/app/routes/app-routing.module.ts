d39b0250a114a04d4c2bba124d4ea04d00ec0918
claudiu9379 <office@bestdeveloper.ro>
2018-06-19 12:38:27 +0300

    'd'

--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -68,7 +68,7 @@ const appRoutes: Routes = [
     RouterModule.forRoot(
       appRoutes,
       {
-        enableTracing: true, // <-- debugging purposes only
+        enableTracing: false, // <-- debugging purposes only
         // preloadingStrategy: SelectivePreloadingStrategy,
 
       }
