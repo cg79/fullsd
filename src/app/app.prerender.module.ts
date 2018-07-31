08ae43cdca5e78d4e1c416c9e931f28bf1b7a537
claudiu9379 <office@bestdeveloper.ro>
2018-07-31 18:04:19 +0300

    seo

--- src/app/app.prerender.module.ts
+++ src/app/app.prerender.module.ts
@@ -4,15 +4,16 @@
 import { NgModule } from '@angular/core';
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
-import { ServerModule } from '@angular/platform-server';
+import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
 
 @NgModule({
   imports: [
     ServerModule,
     AppModule,
+    ServerTransferStateModule
   ],
   bootstrap: [
     AppComponent
   ]
 })
-export class AppServerModule { }
+export class AppPrerenderModule  { }
