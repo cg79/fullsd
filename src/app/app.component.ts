4dc3a8ed7b85aaaa074bca567a2746471ea698cb
claudiu9379 <office@bestdeveloper.ro>
2018-08-24 15:22:29 +0300

    live

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpWrapperService} from "./services/http/httpService";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+
+  constructor(private httpService: HttpWrapperService
+  )
+  {
+  }
+
+  ngOnInit(): void {
+    this.httpService.postJson('/api/pub', {
+      proxy: {
+        method:"add",
+        module: "log"
+      }
+    });
+  }
+
+
 }
